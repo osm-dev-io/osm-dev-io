9a56560497a6dc7b76cf35863c66ec30fd9630ac
Seongmin Oh <osm.dev.io@gmail.com>
2026-06-14 15:14:49 +0900

    글 작성 기능 구현

--- apps/admin/src/app/posts/new/page.tsx
+++ apps/admin/src/app/posts/new/page.tsx
@@ -2,9 +2,54 @@
 
 import TextEditor from "@/components/TextEditor";
 import { useState } from "react";
+import { Button, Group, Stack, TextInput } from "@mantine/core";
+import { useMutation } from "@tanstack/react-query";
+import { JSONContent } from "@tiptap/react";
 
 export default function NewPostPage() {
+  const [title, setTitle] = useState("");
   const [content, setContent] = useState({});
 
-  return <TextEditor value={content} onChange={setContent} />;
+  const mutation = useMutation({
+    mutationFn: (vars: { title: string; content: JSONContent }) => {
+      return fetch("http://localhost:8080/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          content,
+        }),
+      });
+    },
+  });
+
+  return (
+    <Stack>
+      <TextInput
+        variant="unstyled"
+        placeholder="제목"
+        size="xl"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        styles={{
+          input: {
+            fontSize: "var(--mantine-h1-font-size)",
+            fontWeight: "var(--mantine-h1-font-weight)",
+          },
+        }}
+      />
+      <TextEditor value={content} onChange={setContent} />
+      <Group justify="end">
+        <Button
+          onClick={() => {
+            mutation.mutate({ title, content });
+          }}
+        >
+          저장
+        </Button>
+      </Group>
+    </Stack>
+  );
 }
