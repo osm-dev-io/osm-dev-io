18532d8f193bcb396eadceb46f5ea8acea758581
Seongmin Oh <osm.dev.io@gmail.com>
2026-06-14 15:28:56 +0900

    createTheme 모듈 스코프로 이동

--- apps/admin/src/components/Providers.tsx
+++ apps/admin/src/components/Providers.tsx
@@ -5,12 +5,12 @@ import { createTheme, MantineProvider } from "@mantine/core";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { getQueryClient } from "@/queries/getQueryClient";
 
-export default function Providers({ children }: { children: ReactNode }) {
-  const theme = createTheme({
-    white: "#f8f9f6",
-    black: "#0a0a0a",
-  });
+const theme = createTheme({
+  white: "#f8f9f6",
+  black: "#0a0a0a",
+});
 
+export default function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
 
   return (
