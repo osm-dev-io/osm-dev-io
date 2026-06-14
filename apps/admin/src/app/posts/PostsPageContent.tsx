95eae4b6adac2f47934e9b1711954894cbab7e8c
Seongmin Oh <osm.dev.io@gmail.com>
2026-06-14 13:40:37 +0900

    새 글 쓰기 페이지 추가

--- apps/admin/src/app/posts/PostsPageContent.tsx
+++ apps/admin/src/app/posts/PostsPageContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Stack, Title } from "@mantine/core";
+import { Button, Stack, Title } from "@mantine/core";
+import NextLink from "next/link";
 import usePosts from "@/hooks/usePosts";
 
 export default function PostsPageContent() {
@@ -8,6 +9,9 @@ export default function PostsPageContent() {
   return (
     <Stack>
       <Title>게시글 관리</Title>
+      <Button component={NextLink} href="/posts/new" w={200}>
+        새 글 쓰기
+      </Button>
       {posts?.map((post) => (
         <Title key={post.id}>{post.title}</Title>
       ))}
